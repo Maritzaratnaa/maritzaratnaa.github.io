32d74465f5f3736fada06ae358abffdf4b57deff
Maritzaratnaa <144192962+Maritzaratnaa@users.noreply.github.com>
2024-09-29 20:14:06 +0700

    Update tugas4.php

--- Pemrograman-Web/Tugas/tugas4.php
+++ Pemrograman-Web/Tugas/tugas4.php
@@ -32,7 +32,7 @@ deskripsi   : Membuat web form input data menggunakan PHP
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="tugas4.css">
     <title>Form</title>
  </head>
  <body>
